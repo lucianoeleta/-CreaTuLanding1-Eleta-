236b68405d57fa88219f7f0ef01cf11d78489ed6
lucianoeleta <167701036+lucianoeleta@users.noreply.github.com>
2024-09-23 22:55:47 -0300

    Update cartwidget.jsx

--- my-app/src/componentes/cartwidget.jsx
+++ my-app/src/componentes/cartwidget.jsx
@@ -4,7 +4,7 @@ function CartWidget() {
   return (
     <div style={{ display: 'flex', alignItems: 'center' }}>
       <img src="https://cdn-icons-png.flaticon.com/512/5412/5412512.png" alt="Carrito" style={{ height: '24px' }} />
-      <span style={{ marginLeft: '8px' }}>3</span> 
+      <span style={{ marginLeft: '8px' }}>/span> 
     </div>
   );
 }
